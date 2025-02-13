7451d3f1bf9348644a40714a5c815d64ebb93f81
Christopher Novo <cnovo1@ewu.edu>
2025-02-13 03:32:33 +0000

    sonic-google-doodle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log('OK');
+gsap.to(".googleLetter",{
+    duration: 1,     // Duration of each animation
+   y: 50,           // Move each letter 50 pixels on the y-axis
+   stagger: 0.2,    // Stagger start time by 0.2 seconds for each letter
+   repeat: -1,
+   ease: "power1.inOut"
+ });
